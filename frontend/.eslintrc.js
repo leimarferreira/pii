b1647d0a39393beff4f1347e32de89bbd7a483cc
Leimar <leimarferreira100@gmail.com>
2022-04-05 15:55:05 -0300

    [front] Configurando eslint.

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-var": "error",
+        "no-console": "warn",
+        "prefer-arrow-callback": "warn",
+        "react/react-in-jsx-scope": "off",
+    }
+}
