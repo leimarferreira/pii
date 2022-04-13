751fdd8177fd6760d032ed964565fc967e5e397c
Leimar <leimarferreira100@gmail.com>
2022-04-12 21:41:34 -0300

    [back] Adicionando controlador de usuário.

---
+++ backend/src/main/java/pii/controller/UserController.java
@@ -0,0 +1,101 @@
+package pii.controller;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pii.dto.UserDTO;
+import pii.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/")
+	public ResponseEntity<Iterable<UserDTO>> getAll() {
+		var result = userService.findAll();
+		var status = HttpStatus.OK;
+		
+		if (result.isEmpty()) {
+			status = HttpStatus.NO_CONTENT;
+		}
+		
+		return ResponseEntity.status(status).body(result);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<UserDTO>> getById(@PathVariable Long id) {
+		var result = userService.findById(id);
+		var status = HttpStatus.OK;
+		
+		if (result.isEmpty()) {
+			status = HttpStatus.NOT_FOUND;
+		}
+		
+		return ResponseEntity.status(status).body(result);
+	}
+	
+	@GetMapping("/{email}")
+	public ResponseEntity<Optional<UserDTO>> getByEmail(@PathVariable @Size(max = 100) String email) {
+		var result = userService.findByEmail(email);
+		var status = HttpStatus.OK;
+		
+		if (result.isEmpty()) {
+			status = HttpStatus.NOT_FOUND;
+		}
+		
+		return ResponseEntity.status(status).body(result);
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Optional<UserDTO>> post(@Valid @RequestBody UserDTO user) {
+		var result = userService.save(user);
+		var status = HttpStatus.OK;
+		
+		if (result.isEmpty()) {
+			status = HttpStatus.BAD_REQUEST;
+		}
+		
+		return ResponseEntity.status(status).body(result);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Optional<UserDTO>> update(@PathVariable Long id, @Valid @RequestBody UserDTO user) {
+		var result = userService.update(id, user);
+		var status = HttpStatus.OK;
+		
+		if (result.isEmpty()) {
+			status = HttpStatus.NOT_FOUND;
+		}
+		
+		return ResponseEntity.status(status).body(result);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Optional<Void>> delete(@PathVariable Long id) {
+		var result = userService.delete(id);
+		var status = HttpStatus.OK;
+		
+		if (!result) {
+			status = HttpStatus.NOT_FOUND;
+		}
+		
+		return ResponseEntity.status(status).body(Optional.empty());
+	}
+}
