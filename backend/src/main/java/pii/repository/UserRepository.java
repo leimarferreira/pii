8e1adc753459adf99c039efd7c68b3fd12b2c3e9
Leimar <leimarferreira100@gmail.com>
2022-04-21 20:38:18 -0300

    [back] Refatorando camada de dados do usuário.

--- backend/src/main/java/pii/repository/UserRepository.java
+++ backend/src/main/java/pii/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package pii.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -8,11 +9,16 @@ import pii.model.User;
 
 @Repository
 public interface UserRepository {
-	
+
 	public List<User> findAll();
-	public User findById(Long id);
-	public User findByEmail(String email);
-	public User save(User user);
-	public User update(Long id, User user);
+
+	public Optional<User> findById(Long id);
+
+	public Optional<User> findByEmail(String email);
+
+	public Optional<User> save(User user);
+
+	public Optional<User> update(Long id, User user);
+
 	public boolean delete(Long id);
 }
