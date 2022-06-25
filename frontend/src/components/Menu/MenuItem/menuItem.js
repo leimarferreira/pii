0be177806211226a5878d35d4f729bd41126f1f3
Leimar <leimarferreira100@gmail.com>
2022-06-24 22:12:50 -0300

    [front] Adicionando componente para menu.

---
+++ frontend/src/components/Menu/MenuItem/menuItem.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import "./menuItem.css";
+
+const MenuItem = ({ title, icon, onClick, className }) => {
+  const handleMenuItemClick = () => {
+    onClick();
+  };
+
+  return (
+    <div
+      className={`menu-item ${className}`}
+      onClick={onClick ? handleMenuItemClick : () => {}}
+    >
+      <span className="menu-item-icon">{icon}</span>
+      <span className="menu-item-title">{title}</span>
+    </div>
+  );
+};
+
+export default MenuItem;
